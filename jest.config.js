d135cefe83db018883a32e0b58d34308ab9d32bc
Lucas Calebe <lucascalebe07@hotmail.com>
2022-03-14 12:18:49 -0300

    chore: add jest env to eslint config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    roots: ['<rootDir>/src'],
+  roots: ['<rootDir>/src'],
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   testEnvironment: 'node',
