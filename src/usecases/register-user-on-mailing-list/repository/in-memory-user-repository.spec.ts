230da432f8520a019e6cbab4d848bc7c7e91cf6d
Lucas Calebe <lucascalebe07@hotmail.com>
2022-03-16 12:40:11 -0300

    feat: addition, existence and finding of user

--- src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -8,4 +8,14 @@ describe('In memory User repository', () => {
     const user = await userRepo.findUserByEmail('any@email.com')
     expect(user).toBeNull()
   })
+
+  test('should return user if is found in the repository', async () => {
+    const users: UserData[] = []
+    const name = 'any_name'
+    const email = 'any@email.com'
+    const userRepo = new InMemoryUserRepository(users)
+    await userRepo.add({ name, email })
+    const user = await userRepo.findUserByEmail(email)
+    expect(user.name).toBe(name)
+  })
 })
