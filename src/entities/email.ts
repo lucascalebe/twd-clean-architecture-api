977b5ec4678d808fff7aa523be0bbfc82581dd34
Lucas Calebe <lucascalebe07@hotmail.com>
2022-03-16 18:13:49 -0300

    feat: validate empty domain and length largest than 63 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -6,8 +6,17 @@ export class Email {
     if (email.length > 320) {
       return false
     }
-    const [local] = email.split('@')
+    const [local, domain] = email.split('@')
     if (local.length > 64 || local.length === 0) {
+      return false
+    }
+    if (domain.length > 255 || domain.length === 0) {
+      return false
+    }
+    const domainParts = domain.split('.')
+    if (domainParts.some(function (part) {
+      return part.length > 63
+    })) {
       return false
     }
 
