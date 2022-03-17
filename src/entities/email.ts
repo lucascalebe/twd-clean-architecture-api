dc14f56c73e5ccaf3c4a0e69cb9e214ad5343be6
Lucas Calebe <lucascalebe07@hotmail.com>
2022-03-17 11:43:20 -0300

    feat: verify e-mail address when creating user

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,4 +1,19 @@
+import { Either, left } from './../shared/either'
+import { InvalidEmailError } from './errors/invalid-email-error'
 export class Email {
+  private readonly email: string
+
+  constructor (email: string) {
+    this.email = email
+  }
+
+  static create (email: string): Either<InvalidEmailError, Email> {
+    if (Email.validate(email)) {
+      return right(new Email(email))
+    }
+    return left(new InvalidEmailError())
+  }
+
   static validate (email: string): boolean {
     if (!email) {
       return false
@@ -30,4 +45,7 @@ export class Email {
 
     return true
   }
+}
+function right (arg0: Email): Either<InvalidEmailError, Email> {
+  throw new Error('Function not implemented.')
 }
