ecb7c3b1c6c3d6cc4b5a6f276e3111819af70252
Lucas Calebe <lucascalebe07@hotmail.com>
2022-03-28 14:20:05 -0300

    test: improve error classes

--- src/entities/user.spec.ts
+++ src/entities/user.spec.ts
@@ -15,11 +15,11 @@ describe('User domain entity', () => {
     expect(error.message).toEqual('Invalid name: ' + invalidName + '.')
   })
 
-  // test('should not create user with invalid name (too many characters)', () => {
-  //   const invalidName = '0'.repeat(257)
-  //   const error = User.create({ name: invalidName, email: 'any@mail.com' })
-  //   expect(error).toEqual(left(new InvalidNameError()))
-  // })
+  test('should not create user with invalid name (too many characters)', () => {
+    const invalidName = '0'.repeat(257)
+    const error = User.create({ name: invalidName, email: 'any@mail.com' }).value as Error
+    expect(error.message).toEqual('Invalid name: ' + invalidName + '.')
+  })
 
   test('should create user with valid data', () => {
     const validName = 'any_name'
