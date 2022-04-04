b4aa543d5a68dc6ed46bbae1f955a8f8406082e1
Lucas Calebe <lucascalebe07@hotmail.com>
2022-04-04 11:34:54 -0300

    feat: apply cors enabling middleware

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,6 +1,9 @@
 import { bodyParser } from '@/main/config/middleware/body-parser'
+import { cors } from '@/main/config/middleware/cors'
+
 import { Express } from 'express'
 
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
