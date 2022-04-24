ac0ce3e718e0bc14a2096e1761590285763c612b
Lucas Calebe <lucascalebe07@hotmail.com>
2022-04-24 11:51:13 -0300

    test: connect and disconnect to mongodb

--- test/main/routes/register-route.test.ts
+++ test/main/routes/register-route.test.ts
@@ -1,7 +1,19 @@
+import { MongoHelper } from '@/external/repositories/mongodb/helper'
 import app from '@/main/config/app'
 import request from 'supertest'
 
 describe('Register route', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    MongoHelper.clearCollection('users')
+  })
   test('should return an account on success', async () => {
     app.post('/test_cors', (req, res) => {
       res.send()
