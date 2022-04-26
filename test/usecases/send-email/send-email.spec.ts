5abb65b289517e97e7d643f600d851dea53dcdc9
Lucas Calebe <lucascalebe07@hotmail.com>
2022-04-26 10:58:02 -0300

    test: return error when mail sending fails

--- test/usecases/send-email/send-email.spec.ts
+++ test/usecases/send-email/send-email.spec.ts
@@ -1,3 +1,4 @@
+import { left } from './../../../src/shared/either'
 import { Left, Right } from '@/shared/either'
 import { MailServiceError } from '@/usecases/errors/mail-service-error'
 import { Either, right } from '@/shared'
@@ -36,19 +37,32 @@ class MailServiceStub implements EmailService {
   }
 }
 
+class MailServiceErrorStub implements EmailService {
+  async send (emailOptions: EmailOptions): Promise<Either<MailServiceError, EmailOptions>> {
+    return left(new MailServiceError())
+  }
+}
+
 describe('Send email to user', () => {
   test('should email user with valid name and email address', async () => {
     const mailServiceStub = new MailServiceStub()
-    const useCase = await new SendEmail(mailOptions, mailServiceStub)
+    const useCase = new SendEmail(mailOptions, mailServiceStub)
     const response = await useCase.perform({ name: toName, email: toEmail })
     expect(response).toBeInstanceOf(Right)
   })
 
   test('should not try email with invalid email address', async () => {
     const mailServiceStub = new MailServiceStub()
-    const useCase = await new SendEmail(mailOptions, mailServiceStub)
+    const useCase = new SendEmail(mailOptions, mailServiceStub)
     const invalidEmail = 'invalid_email'
     const response = await useCase.perform({ name: toName, email: invalidEmail })
     expect(response).toBeInstanceOf(Left)
+  })
+
+  test('should return error when email service fails', async () => {
+    const mailServiceErrorStub = new MailServiceErrorStub()
+    const useCase = new SendEmail(mailOptions, mailServiceErrorStub)
+    const response = await useCase.perform({ name: toName, email: toEmail })
+    expect(response.value).toBeInstanceOf(MailServiceError)
   })
 })
